0fa468b95b0540e51ff9d980f07070ec42448c70
Jedi-Coder1 <126493503+SnappyCarp@users.noreply.github.com>
2023-03-06 12:35:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+DESCRIPTION = 'Easy YouTube Notifications'
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    l_d = "\n" + fh.read()
+
 
 setup(
     name='YtWebhookNotification',
     url='https://github.com/SnappyCarp/YtWebhookNotification',
     author='JediMinecraft1',
     author_email='edmund.spielman@outlook.com',
-    # Needed to actually package something
-    packages=['measure'],
+    packages=find_packages(),
     # Needed for dependencies
-    install_requires=['discord-webhook'],
-    # *strongly* suggested for sharing
+    install_requires=['discord-webhook', 'requests', 're'],
     version='0.1',
     license='MIT',
-    description='Easy YouTube Notifications',
-    long_description=open('README.md').read(),
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=l_d,
 )
